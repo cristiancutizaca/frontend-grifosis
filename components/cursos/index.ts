eff742edff38cb6e641b9beea732fff577b81214
cristiancutizaca <cutizacamachacacristian@gmail.com>
2025-06-23 13:44:01 -0500

    eliminacion dell whatsap

--- components/cursos/index.ts
+++ components/cursos/index.ts
@@ -7,12 +7,10 @@ const styles = {
     xPaddings: 'sm:px-16 px-6',
     topPaddings: 'sm:pt-16 xs:pt-8 pt-12',
     bottomPaddings: 'sm:pb-16 xs:pb-8 pb-12',
-
     flexCenter: 'flex justify-center items-center',
     flexStart: 'flex justify-start items-start',
     flexEnd: 'flex justify-end',
     navPadding: 'pt-[98px]',
-
     // hero section
     heroHeading:
         'font-bold lg:text-[144px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white',
